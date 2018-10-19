5f990492e5f5174947fdd00869d6049a0ae75a10
Ana Pecini <anna.pecini@yahoo.com>
2018-10-19 14:50:28 +0200

    Separate classes for models

--- WebService/Models/CategoryPostAndPutModel.cs
+++ WebService/Models/CategoryPostAndPutModel.cs
@@ -5,16 +5,4 @@ namespace WebService.Models
         public string Name { get; set; }
         public string Description { get; set; }
     }
-
-    public class ProductGetModel
-    {
-        public string Name { get; set; }
-        public string CategoryName { get; set; }
-    }
-    
-    public class ListProductGetModel
-    {
-        public string ProductName { get; set; }
-        public string CategoryName { get; set; }
-    }
 }
---
+++ WebService/Models/ListProductGetModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Models
+{
+    public class ListProductGetModel
+    {
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

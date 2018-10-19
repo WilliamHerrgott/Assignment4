539ee209ad0d3f32cd7362ee786735e4cc3f51e8
William Herrgott <herrgott.william@gmail.com>
2018-10-19 14:21:29 +0200

    Model and some broken stuff

--- WebService/Controllers/CategoriesController.cs
+++ WebService/Controllers/CategoriesController.cs
@@ -1,18 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assignment4;
+﻿using Assignment4;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+using WebService.Models;
 
 namespace WebService.Controllers
 {
     [Route("api/categories")]
     [ApiController]
-    public class CategoriesController : Controller
-    {
+    public class CategoriesController : Controller {
+        private readonly DataService _dataService;
 
+        public CategoriesController(DataService dataService) {
+            _dataService = dataService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories() {
+            var data = _dataService.GetCategories();
+
+            return Ok(data);
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _dataService.GetCategory(id);
+            
+            if (category == null) {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(CategoryPostAndPutModel category)
+        {
+            _dataService.CreateCategory(category.Name, category.Description);
+            return Ok();
+        }
     }
 }
--- WebService/Controllers/ProductsController.cs
+++ WebService/Controllers/ProductsController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assignment4;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebService.Controllers
 {
